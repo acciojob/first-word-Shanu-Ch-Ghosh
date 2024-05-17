36f4b77db7fb12856811f4df72433b9181891516
Shanu Chandra Ghosh <166943936+Shanu-Ch-Ghosh@users.noreply.github.com>
2024-05-17 23:18:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function firstWord(s) {
-  // your code here
+  // Trim leading spaces
+  const trimmedStr = s.trimStart();
+  
+  // Check if the trimmed string is empty
+  if (trimmedStr === '') {
+    return '';
+  }
+  
+  // Find the first space
+  const spaceIndex = trimmedStr.indexOf(' ');
+  
+  // If there is no space, return the entire trimmed string
+  if (spaceIndex === -1) {
+    return trimmedStr;
+  }
+  
+  // Return the substring up to the first space
+  return trimmedStr.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
